f2cc3cd064a281ae0d201d8f27407ab5a7b59d0b
postBG <profile2697@gmail.com>
2016-02-05 17:58:03 +0900

    잘못된 코드는 지우기

--- WebContent/js/input_validation.js
+++ WebContent/js/input_validation.js
@@ -1,5 +1,3 @@
-var errorCode = [0, -1, -2];
-
 $(document).ready(function(){
   
   $("#username").on("input", function(){
@@ -17,8 +15,13 @@ $(document).ready(function(){
   });
   
   $("#email").on("input", function(){
+    var email=$(this).val();
     var reg_email=/^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{0}[-A-Za-z0-9_.]*$/;
-    if($(this).val().search(reg_email) == -1){
+    
+    if(email.length == 0){
+      $("#password_error").html("이메일을 입력해주세요.");
+    }
+    else if(email.search(reg_email) == -1){
       $("#password_error").html("이메일 포멧이 올바르지 않습니다.");
     }
     else{
@@ -26,22 +29,4 @@ $(document).ready(function(){
     }
   });
   
-  $("#user_info").submit(function(event){
-    if(!isUserNameValid() || !isEmailValid()){
-      event.preventDefault();
-    }
-  });
 });
-
-function isUserNameValid(){
-  var userNameLength = $("#username").val().length;
-  
-  return userNameLength == 0 || userNameLength < 2;
-}
-
-function isEmailValid(){
-  var email = $("#email").val();
-  var reg_email=/^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{0}[-A-Za-z0-9_.]*$/;
-  
-  return email.search(reg_email) != -1;
-}
